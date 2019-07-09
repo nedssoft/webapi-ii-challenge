ce7d1bb2e2761a611b61efff73dad459fec88342
Orie Chinedu <oriechinedu@gmail.com>
2019-07-09 14:45:31 +0100

    Implement endpoint to create post comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,46 +1,92 @@
 const Post = require("../data/db");
 
 const respondSuccess = (statusCode, data, message = null, res) => {
-  return res
-    .status(statusCode)
-    .json({
-      status: "success",
-      ...(message && { message }),
-      data
-    });
-}
+  return res.status(statusCode).json({
+    status: "success",
+    ...(message && { message }),
+    data
+  });
+};
 const respondError = (statusCode, error = null, message = null, res) => {
-  return res
-    .status(statusCode)
-    .json({
-      status: "error",
-      ...(message && {errorMessage: message }),
-      ...(error && { error }),
-    });
-}
+  return res.status(statusCode).json({
+    status: "error",
+    ...(message && { errorMessage: message }),
+    ...(error && { error })
+  });
+};
 const createPost = async (req, res) => {
   try {
     const { title, contents } = req.body;
 
     if (!title || !contents) {
-        respondError(400, null,"Please provide title and contents for the post." , res)
+      respondError(
+        400,
+        null,
+        "Please provide title and contents for the post.",
+        res
+      );
     } else {
       const post = await Post.insert({
         title,
         contents
-      })
-      if(post) {
-        respondSuccess(201, post, 'Post created successfully', res)
+      });
+      if (post) {
+        respondSuccess(201, post, "Post created successfully", res);
       } else {
-        respondError(500, null,"There was an error while saving the post to the database", res)
+        respondError(
+          500,
+          null,
+          "There was an error while saving the post to the database",
+          res
+        );
       }
     }
   } catch (error) {
-    respondError(500, error,"There was an error while saving the post to the database", res)
+    respondError(
+      500,
+      error,
+      "There was an error while saving the post to the database",
+      res
+    );
+  }
+};
+
+const createPostComment = async (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  try {
+    if (!text) {
+      respondError(400, null, "Provide the text for the comment", res);
+    } else {
+      const post = await Post.findById(id);
+      if (!post.length) {
+        respondError(404, null, "The post with the specified ID does not exist", res);
+      } else {
+        const comment = await Post.insertComment({ text, post_id: id });
+        if (comment) {
+          respondSuccess(201, comment, "Comment created successfully", res);
+        } else {
+          respondError(
+            500,
+            null,
+            "There was an error while saving the comment to the database",
+            res
+          );
+        }
+      }
+    }
+   
+  } catch (error) {
+    respondError(
+      500,
+      error,
+      "There was an error while saving the comment to the database",
+      res
+    );
   }
 };
 
 module.exports = {
-  createPost
-}
-
+  createPost,
+  createPostComment
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
-const { createPost } = require('../controllers')
+const { createPost, createPostComment } = require('../controllers')
 const router = express.Router()
 
 router.post('/', createPost)
 
+router.post('/:id/comments', createPostComment)
 module.exports = {
   router: router
 }
